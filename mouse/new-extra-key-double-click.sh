2f400e1739eab82f531ca1d548512455f38b3d4b
Konstantin Molchanov <kostya.molchanov@gmail.com>
2020-07-11 19:21:08 +0600

    Run evtest as root

--- mouse/new-extra-key-double-click.sh
+++ mouse/new-extra-key-double-click.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+if [ "$(sudo whoami)" != "root" ]; then
+        echo -e "\033[37;1;41mSorry, you are not root.\033[0m"
+        exit
+fi
+
 buttonId=$(xinput --list | grep 'A4TECH USB Device Keyboard' | grep -oE 'id=[0-9]+' | grep -oE '[0-9]+')
 
 if [ -n "$buttonId" ]
 	then
 		xinput --disable $buttonId
 		tEvent=$(xinput --list-props $buttonId | grep -oE "event+[0-9]{1,3}")
-		evtest /dev/input/$tEvent | awk '/KEY_LEFTMETA\), value 1/ {system("xdotool click --repeat 2 1")}'
+		sudo evtest /dev/input/$tEvent | awk '/KEY_LEFTMETA\), value 1/ {system("xdotool click --repeat 2 1")}'
 fi
